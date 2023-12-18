dc91f17b4595b12cd33f1ee5d8bcafff7cb8e05f
nannarae <awsed333@reappay.co.kr>
2023-12-18 14:47:44 +0900

    env: change api key

--- src/constants/chainConfig.ts
+++ src/constants/chainConfig.ts
@@ -74,9 +74,9 @@ export const ethereumConfig: EthereumConfig = {
   chainIdHex: "0xAA36A7",
   chainName: "Sepolia TestNet",
   // for testnet
-  rpcEndpoint: "https://test-eth-rpc.reapchain.org",
-  // for debug
-  // rpcEndpoint: "https://sepolia.infura.io/v3/91032b3fbbf842e7b545289832cf07e0",
+  rpcEndpoint:
+    "https://eth-sepolia.g.alchemy.com/v2/HoLqG30vvtgxqQWWAYSDVJzkNgM7u_-T",
+  // rpcEndpoint: "https://test-eth-rpc.reapchain.org",
   explorerUrl: "https://sepolia.etherscan.io",
   restEndpoint: "https://test-lcd.reapchain.org",
 
